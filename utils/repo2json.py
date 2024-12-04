f21dfdcbab6b41015ae6fad2f61820c99f054e93
Charley Xiao <59205189+Charley-xiao@users.noreply.github.com>
2024-12-04 17:40:15 +0800

    Add node count

--- utils/repo2json.py
+++ utils/repo2json.py
@@ -125,9 +125,11 @@ def process_repo(index, repo, setting=3, num_cpus=None):
             start_time = time.time()
             graph_py = get_dependency_graph(repo_path, 'python')
             print(f"Python graph for {repo_name} built in {time.time() - start_time:.2f} seconds")
+            print(f"number of nodes: {len(graph_py.nodes)}")
             start_time = time.time()
             graph_java = get_dependency_graph(repo_path, 'java')
             print(f"Java graph for {repo_name} built in {time.time() - start_time:.2f} seconds")
+            print(f"number of nodes: {len(graph_java.nodes)}")
             graph = nx.compose(graph_py, graph_java)
             print(f"Combined graph for {repo_name} built")
             with open(cache_path, 'wb') as f:
